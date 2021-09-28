b34345901a86dc1514e0084d46f176613f6c277f
Amanda <amandaspolaor.aluno@unipampa.edu.br>
2021-09-28 00:36:31 -0300

    microsserviço evolucaodb

---
+++ App-Hotel-RPIV-FixingMicroservi-os/Hotelaria/evolucaodb/src/main/java/com/rp4/evolucaodb/EvolucaodbApplication.java
@@ -0,0 +1,13 @@
+package com.rp4.evolucaodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolucaodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolucaodbApplication.class, args);
+    }
+
+}
